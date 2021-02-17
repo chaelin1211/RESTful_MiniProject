0e9cb9fcc3b213f75e9d3026f15adb1c46f1d831
chaelin1211 <cofls749@naver.com>
2021-02-18 02:40:06 +0900

    Add Annotation to Bean

--- demo/src/main/java/com/example/demo/todo_item/TodoBean.java
+++ demo/src/main/java/com/example/demo/todo_item/TodoBean.java
@@ -2,6 +2,12 @@ package com.example.demo.todo_item;
 
 import lombok.*;
 
+@Data
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
 public class TodoBean {
     private String id;
     private String title;
