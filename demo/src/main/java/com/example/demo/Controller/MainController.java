962776a4ef5d394cdbff29d7fb0c184018b4e85e
chaelin1211 <cofls749@naver.com>
2021-02-18 01:12:13 +0900

    Change controller and setting for test

--- demo/src/main/java/com/example/demo/Controller/MainController.java
+++ demo/src/main/java/com/example/demo/Controller/MainController.java
@@ -1,28 +1,16 @@
-package com.example.demo.Controller;
+package com.example.demo.controller;
 
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.ObjectUtils;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
-
-    @RequestMapping(value = "/{action}", method = { RequestMethod.GET, RequestMethod.POST })
-    public ModelAndView actionMethod(@RequestParam Map<String, Object> paramMap, @PathVariable String action,
-            ModelAndView modelAndView) {
-
-        String viewName = "/main";
-               
-        modelAndView.setViewName(viewName);
-        modelAndView.addObject("paramMap", paramMap);        
-        return modelAndView;
+    @RequestMapping("/")
+    @ResponseBody
+    public String index(){
+        return "Hello, Spring";
     }
-
 }
