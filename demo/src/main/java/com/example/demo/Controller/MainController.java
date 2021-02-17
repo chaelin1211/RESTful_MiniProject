e6cdde76c4658f1cca5e80d71466ad29905d6daf
chaelin1211 <cofls749@naver.com>
2021-02-17 23:41:48 +0900

    Add test controller and template

---
+++ demo/src/main/java/com/example/demo/Controller/MainController.java
@@ -0,0 +1,28 @@
+package com.example.demo.Controller;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value = "/{action}", method = { RequestMethod.GET, RequestMethod.POST })
+    public ModelAndView actionMethod(@RequestParam Map<String, Object> paramMap, @PathVariable String action,
+            ModelAndView modelAndView) {
+
+        String viewName = "/main";
+               
+        modelAndView.setViewName(viewName);
+        modelAndView.addObject("paramMap", paramMap);        
+        return modelAndView;
+    }
+
+}
