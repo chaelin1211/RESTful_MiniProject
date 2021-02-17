0cd77d198f291f9759dbd9e62b01bb42337dc7ac
chaelin1211 <cofls749@naver.com>
2021-02-18 02:40:46 +0900

    Rename class Response

---
+++ demo/src/main/java/com/example/demo/todo_item/TodoResponse.java
@@ -0,0 +1,14 @@
+package com.example.demo.todo_item;
+
+import com.example.demo.ApiResponse.commonResponse;
+
+import lombok.*;
+import java.util.List;
+
+public class TodoResponse extends commonResponse<TodoBean> {
+    @Builder
+    public TodoResponse(final TodoBean todoBean, final List<String> errors){
+        super(todoBean);
+        this.setErrores(errors);
+    }    
+}
