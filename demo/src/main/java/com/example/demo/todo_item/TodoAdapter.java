c732101765205dc69d78fad7e2e2e1e3665453d6
chaelin1211 <cofls749@naver.com>
2021-03-01 22:44:45 +0900

    Add class (change  request to bean)

--- demo/src/main/java/com/example/demo/todo_item/TodoAdapter.java
+++ demo/src/main/java/com/example/demo/todo_item/TodoAdapter.java
@@ -8,4 +8,15 @@ public class TodoAdapter {
     public static TodoResponse toTodoResponse(final TodoBean todoBean, final List<String> errors){
         return TodoResponse.builder().todoBean(todoBean).errors(errors).build();
     }    
+
+    public static TodoBean toTodoBean(final TodoRequest todoRequest){
+        if(todoRequest == null){
+            return null;
+        }
+        return  TodoBean.builder()
+            .title(todoRequest.getTitle())
+            .done(todoRequest.isDone())
+            .build();
+
+    }
 }
