d35eea010c644edecb341dad240cc7670c06c494
chaelin1211 <cofls749@naver.com>
2021-03-01 22:47:43 +0900

    Add method (getAll, Create)

--- demo/src/main/java/com/example/demo/todo_item/TodoService.java
+++ demo/src/main/java/com/example/demo/todo_item/TodoService.java
@@ -1,12 +1,27 @@
 package com.example.demo.todo_item;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TodoService {
+    @Autowired
+    private TodoRepository todoRepository;
+
     public TodoBean get(final String id){
-        return TodoBean.builder()
-                        .title("Add an id Validation")
-                        .build();
+        return todoRepository.findById(id).orElse(null);
+    }
+
+    public TodoBean create(final TodoBean todoBean){
+        if(todoBean == null){
+            throw new NullPointerException("Item cannot be null.");
+        }
+        return todoRepository.insert(todoBean);
+    }
+
+    public List<TodoBean> getAll(){
+        return todoRepository.findAll();
     }
 }
