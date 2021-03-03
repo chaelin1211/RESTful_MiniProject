36bb4200d18ebe860be9ff3ca6e2a4406a54e999
chaelin1211 <cofls749@naver.com>
2021-03-04 02:06:34 +0900

    Add React.js and Node.js

--- demo/src/main/java/com/example/demo/todo_item/TodoController.java
+++ demo/src/main/java/com/example/demo/todo_item/TodoController.java
@@ -10,7 +10,6 @@ import java.util.List;
 import java.util.ArrayList;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Getter @Setter
--- demo/src/main/java/com/example/demo/todo_item/TodoRequest.java
+++ demo/src/main/java/com/example/demo/todo_item/TodoRequest.java
@@ -1,14 +1,11 @@
 package com.example.demo.todo_item;
 
-import org.springframework.data.annotation.Id;
-
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class TodoRequest {
-    @Id
     private String id;
     private String title;
     private boolean done;
