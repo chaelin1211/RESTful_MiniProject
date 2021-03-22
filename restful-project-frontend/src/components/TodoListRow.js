333aa1a3712cb60a3090e1332849c4e5df23a9e0
chaelin1211 <cofls749@naver.com>
2021-03-23 02:13:51 +0900

    Change table to div tag

--- restful-project-frontend/src/components/TodoListRow.js
+++ restful-project-frontend/src/components/TodoListRow.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
 
-export default class TodoListRow extends Component{
-    constructor(props) {
-        super(props);
-    }
-  
-    render() {
-      return (
-          <tr>
-            <td>
-              
-            </td>
-            <td>
-                {this.props.item.title}
-            </td>
-          </tr>
-      );
-    }
+export default class TodoListRow extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="form-check todoRow">
+        <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
+        <label className="form-check-label todoTitle" for="flexCheckDefault">
+          {this.props.item.title}
+        </label>
+      </div>
+    );
+  }
 }
