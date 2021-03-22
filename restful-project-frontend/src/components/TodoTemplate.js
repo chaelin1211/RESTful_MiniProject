e26c9a7bafd60c548d1f6383b98f47804515e764
chaelin1211 <cofls749@naver.com>
2021-03-23 02:10:30 +0900

    Create Template

--- restful-project-frontend/src/components/TodoTemplate.js
+++ restful-project-frontend/src/components/TodoTemplate.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 
-export default class TodoTemplate extends Component{
+export default class TodoTemplate extends Component {
     constructor(props) {
         super(props);
     }
-    render(){
-        return (<div className="container todoTemplate">{this.props.children}</div>
+    render() {
+        return (
+            <div className="container todoTemplate">
+                {this.props.children}
+            </div>
         );
     }
 }
