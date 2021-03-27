17edf237498744fbe86d6d5ef07c4f0feed22546
chaelin1211 <cofls749@naver.com>
2021-03-27 11:56:20 +0900

    Add comments and fix typo

--- restful-project-frontend/src/components/TodoListRow.js
+++ restful-project-frontend/src/components/TodoListRow.js
@@ -5,21 +5,20 @@ export default class TodoListRow extends Component {
   constructor(props) {
     super(props);
     this.todoService = new TodoService();
-
   }
 
   handleClick() {
     const todoRequest = {
-      id: this.props.item.id,
-      title: this.props.item.title
+      id: this.props.item.id
     }
 
-    this.todoService.delete(todoRequest,(data) => this.props.todoList.removeTodoListRow(data));
+    this.todoService.delete(todoRequest, (data) => this.props.todoList.removeTodoListRow(data));
   }
 
   render() {
     return (
       <div className="todoRow">
+        {/* 체크박스 svg 태그 (생략) */}
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16">
           <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
           <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z" />
@@ -27,9 +26,11 @@ export default class TodoListRow extends Component {
         {/* <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-circle" viewBox="0 0 16 16">
             <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
           </svg> */}
-        <input className="todoTitle d-inline" value={this.props.item.title} readonly id="title" for="flexCheckDefault"></input>
-        <input value={this.props.item.id} hidden id="id" ></input>
 
+        {/* todo item title */}
+        <input className="todoTitle d-inline" value={this.props.item.title} readonly id="title"></input>
+
+        {/* 삭제 버튼 */}
         <div className="float-right deleteButton" onClick={this.handleClick.bind(this)}>
           <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
             <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
