454b32bec856295c3d2a7e31872e4a29239a1c22
chaelin1211 <cofls749@naver.com>
2021-04-02 19:04:16 +0900

    Change url port 8080 to 5000

--- restful-project-frontend/src/components/TodoService.js
+++ restful-project-frontend/src/components/TodoService.js
@@ -2,35 +2,35 @@ import axios from 'axios';
 
 export default class TodoService {
     getAll(callback) {
-        const url = 'http://localhost:8080/todo';
+        const url = 'http://localhost:5000/todo';
         axios.get(url).then((response) => callback(response.data))
             .catch(e => {
                 console.log(e);
             });
     }
     get(request, callback) {
-        const url = 'http://localhost:8080/todo/'+request.id;
+        const url = 'http://localhost:5000/todo/'+request.id;
         axios.get(url).then((response) => callback(response.data))
             .catch(e => {
                 console.log(e);
             });
     }
     post(request, callback) {
-        const url = 'http://localhost:8080/todo';
+        const url = 'http://localhost:5000/todo';
         axios.post(url, request).then((response) => callback(response.data))
             .catch(function (error) {
                 console.log(error);
             });
     }
     delete(request, callback) {
-        const url = 'http://localhost:8080/todo/'+request.id;
+        const url = 'http://localhost:5000/todo/'+request.id;
         axios.delete(url).then((response) => callback(response.data))
             .catch(function (error) {
                 console.log(error);
             });
     }
     update(request, callback) {
-        const url = 'http://localhost:8080/todo';
+        const url = 'http://localhost:5000/todo';
         axios.patch(url, request).then((response) => callback(response.data))
             .catch(function (error) {
                 console.log(error);
